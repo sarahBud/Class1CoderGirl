f8f0605c1b32cb81be6f88d5f5dff783502c60c0
sebudwell@gmail.com <sebudwell@gmail.com>
2019-01-28 18:53:49 -0600

    Add project files.

---
+++ Class1CoderGirl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Class1CoderGirl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            string standardGreeting = "Hello, " + name + ".";
+
+           if (name == "Amar")
+            {
+                Console.WriteLine(standardGreeting + "You have nice shoes.");
+            }
+            
+            else if(name == "Brandy")
+            {
+
+                Console.WriteLine(standardGreeting + "You are awesome!");
+
+            }
+            else
+            {
+                Console.WriteLine(standardGreeting);
+            }
+            Console.ReadLine();
+            
+
+
+
+         
+        }
+    }
+}
